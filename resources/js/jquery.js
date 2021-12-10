c9a3cab2c53d055464b3487fc6ae4385e7a0a631
LawKhunMaine <kyawlay678po@gmail.com>
2021-12-10 22:38:30 +0630

    adding animation

--- resources/js/jquery.js
+++ resources/js/jquery.js
@@ -10,4 +10,55 @@ $(document).ready(function () {
     },
     { offset: "60%" }
   );
+  
+
+  // scrolling animation
+
+  $(".js--wp-1").waypoint(function (direction) {
+   $(".js--wp-1").addClass("animate__zoomIn");
+  },{offset:"100%"});
+
+  $(".js--wp-2").waypoint(function (direction) {
+    $(".js--wp-2").addClass("animate__zoomIn"); 
+  },{offset:"100%"});
+
+  $('.js--wp-3').waypoint(function(direction){
+    $('.js--wp-3').addClass('animate__fadeInUp');
+  },{offset:'100%'})
+
+  $('.js--wp-box').waypoint(function(direction){
+    $('.js--wp-box').addClass('animate__fadeInBottomRight');
+  },{offset:'100%'})
+
+  $('.js--wp-rules-img').waypoint(function(direction){
+    $('.js--wp-rules-img').addClass('animate__fadeInLeft');
+  },{offset:'100%'})
+
+  $('.js--wp-rules-text').waypoint(function(direction){
+    $('.js--wp-rules-text').addClass('animate__fadeInUp');
+  },{offset:'100%'})
+
+  $('.js--wp-gallery-box-1').waypoint(function(direction){
+    $('.js--wp-gallery-box-1').addClass('animate__fadeInBottomRight');
+  },{offset:'100%'})
+
+  $('.js--wp-gallery-box-2').waypoint(function(direction){
+    $('.js--wp-gallery-box-2').addClass('animate__fadeInBottomLeft');
+  },{offset:'100%'})
+
+   // Scrolling btn
+
+   $('.js--btn-gallery').click(function (){
+    $('html,body').animate({scrollTop:$('.js--section-gallery').offset().top},1000); 
+})
+
+   $('.js--btn-home').click(function(){
+    $('html,body').animate({scrollTop:$('.js--section-main').offset().top},500)
+})
+
+   $('.js--btn-features').click(function(){
+    $('html,body').animate({scrollTop:$('.js--section-feature').offset().top},500)
+})
+
 });
+
